5cebf054ec30bf382089192eb2b9a5acd011dc1e
Pasonnn <jimmyson2004@gmail.com>
2024-11-15 22:16:27 +0700

    Fail Connect Mongodb

--- microservices/category/api/routes/categoryRoutes.js
+++ microservices/category/api/routes/categoryRoutes.js
@@ -1,22 +1,21 @@
-// api/routes/categoryRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
+const { authenticateUser } = require('../middlewares/authMiddleware'); // Correct import
 
 // Route to create a new category
-router.post('/create', categoryController.createCategory);
+router.post('/create', authenticateUser, categoryController.createCategory);
 
 // Route to get all categories
-router.get('/all', categoryController.getAllCategories);
+router.get('/all', authenticateUser, categoryController.getAllCategories);
 
 // Route to get a single category by ID
-router.get('/:id', categoryController.getCategoryById);
+router.get('/:id', authenticateUser, categoryController.getCategoryById);
 
 // Route to update a category by ID
-router.put('/:id', categoryController.updateCategory);
+router.put('/:id', authenticateUser, categoryController.updateCategory);
 
 // Route to delete a category by ID
-router.delete('/:id', categoryController.deleteCategory);
+router.delete('/:id', authenticateUser, categoryController.deleteCategory);
 
 module.exports = router;
